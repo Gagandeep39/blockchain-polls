e8db3152f421b5d010d03f17dfd932ef266d460e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-29 08:58:38 +0000

    Created poll and voter model

---
+++ frontend/src/app/models/poll.model.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-11-29 08:24:17
+ * @modify date 2020-11-29 08:24:17
+ * @desc Poll Model
+ */
+export interface Poll {
+  id: number; // 1001
+  question: string; // Which day of the week you like the most?
+  results: number[]; // [0, 0, 0, 5, 2]
+  options: string[]; // [Sunday, Monday, Thursday...]
+  thumbnail: string; // https;//image.png
+}
---
+++ frontend/src/app/models/voter.model.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-11-29 08:27:02
+ * @modify date 2020-11-29 08:27:02
+ * @desc Voter model
+ */
+export interface Voter {
+  id: string; // 0x45HJSH
+  voted: number[]; //12
+}
