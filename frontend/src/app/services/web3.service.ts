aa16d8e2fac5d38a38563ddd2dc8bbc0ba4cb32c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-30 18:59:50 +0530

    Updated web 3 service

--- frontend/src/app/services/web3.service.ts
+++ frontend/src/app/services/web3.service.ts
@@ -5,11 +5,12 @@
  * @modify date 2020-11-30 07:32:40
  * @desc Communicates ith web3
  */
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import Web3 from 'web3';
 import { Contract } from 'web3-eth-contract';
 const contractAbi = require('../../assets/ContractABI.json');
+import { Observable } from 'rxjs';
 
 declare var window: any;
 
@@ -22,7 +23,7 @@ export class Web3Service {
   // Go to metamask -> Account -> Copy the address
   private contractAddress = environment.contractAddress;
 
-  constructor() {
+  constructor(private zone: NgZone) {
     if (window.web3) {
       this.web3 = new Web3(window.ethereum);
       // contractAbi can be fetched from remix
@@ -42,7 +43,25 @@ export class Web3Service {
 
   async executeTransaction(functionName, ...args: any[]): Promise<void> {
     const acc = await this.getAccount();
+    this.contract.methods[functionName](...args).send({ from: acc });
+  }
+
+  async call(functionName: string, ...args: any[]) {
+    const acc = await this.getAccount();
     return this.contract.methods[functionName](...args).send({ from: acc });
   }
 
+  onEvents(event: string) {
+    return new Observable((observer) => {
+      this.contract.events[event]().on('data', (data) => {
+        // THIS MUST RUN INSIDE ANGULAR ZONE AS IT'S LISTENING FOR 'ON'
+        this.zone.run(() => {
+          observer.next({
+            event: data.event,
+            payload: data.returnValues,
+          });
+        });
+      });
+    });
+  }
 }
